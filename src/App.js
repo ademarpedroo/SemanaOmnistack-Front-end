769e32a4548c6f2ecead022d936a8a7f90c44f1a
ademarpedroo <ademarpedro2012@gmail.com>
2020-04-01 13:30:58 -0300

    Front-end Semana Omnistack

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css'
+import Routes from './routes';
+
+// JSX (JAVASCRIPT XML)
+function App() {  
+
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
